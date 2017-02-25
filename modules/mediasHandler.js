59dbde338d3f0b045e6b07f8d8c0f53b1f4b0f96
Oussama Barkouki <oussama1598@gmail.com>
2017-02-25 15:22:03 +0000

    Added video size to the home page

--- modules/mediasHandler.js
+++ modules/mediasHandler.js
@@ -2,23 +2,36 @@ const async = require("async");
 const tvShowsData = require("./tvshowsData");
 const Q = require("q");
 const path = require("path");
+const fs = require("fs");
+const config = require("./config");
+const filesize = require("filesize");
 
 function getMedias() {
     return Q.Promise(resolve => {
         let episodes = {};
         async.forEach(global.Files, (file, cb) => {
-            const metadata = tvShowsData.parseFromFilename(file);
+            const metadata = tvShowsData.parseFromFilename(file),
+                fullPath = path.join(config("SAVETOFOLDER"), file);
+
             if (!episodes[metadata.serieName]) episodes[metadata.serieName] = [];
 
-            episodes[metadata.serieName].push({
-                filename: path.basename(file),
-                serieName: metadata.serieName,
-                episode: parseInt(metadata.episode),
-                season: parseInt(metadata.season),
-                streamUrl: encodeURI(`/stream/${file}`),
-                thumbUrl: encodeURI(`/thumb/${file}`)
-            })
+            fs.stat(fullPath, (err, stats) => {
+                if (err) return;
 
+                episodes[metadata.serieName].push({
+                    filename: path.basename(file),
+                    serieName: metadata.serieName,
+                    episode: parseInt(metadata.episode),
+                    season: parseInt(metadata.season),
+                    streamUrl: encodeURI(`/stream/${file}`),
+                    thumbUrl: encodeURI(`/thumb/${file}`),
+                    fileDetails: {
+                        fullPath,
+                        size: filesize(stats["size"])
+                    }
+                })
+
+            })
             cb();
         }, err => {
             tvShowsData.getEpisodeDataByQuery(episodes)
